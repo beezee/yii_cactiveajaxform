abd04768d97e59f55938585b49321210028ab8ed
beezee <brian.zeligson@gmail.com>
2012-12-30 14:42:06 -0500

    cleanup and add support for onSubmit

--- CActiveAjaxForm.php
+++ CActiveAjaxForm.php
@@ -15,19 +15,6 @@ class CActiveAjaxForm extends CActiveForm
                 $this->focus="#".CHtml::activeId($this->focus[0],$this->focus[1]);
         echo CHtml::endForm();
         $cs=Yii::app()->clientScript;
-        if(!$this->enableAjaxValidation && !$this->enableClientValidation || empty($this->attributes))
-        {
-                if($this->focus!==null)
-                {
-                        $cs->registerCoreScript('jquery');
-                        $cs->registerScript('CActiveForm#focus',"
-                                if(!window.location.hash)
-                                        $('".$this->focus."').focus();
-                        ");
-                }
-                return;
-        }
-
         $options=$this->clientOptions;
         if(isset($this->clientOptions['validationUrl']) && is_array($this->clientOptions['validationUrl']))
                 $options['validationUrl']=CHtml::normalizeUrl($this->clientOptions['validationUrl']);
@@ -38,13 +25,14 @@ class CActiveAjaxForm extends CActiveForm
             ? preg_replace('/^js\:/', '', $options['afterValidate'], 1) : 'function() {return;}';
         $afterSaveCB = isset($options['afterSave'])
             ? preg_replace('/^js\:/', '', $options['afterSave'], 1) : 'function(r) { return; }';
+        $onSubmitCB = isset($options['onSubmit'])
+            ? preg_replace('/^js\:/', '', $options['onSubmit'], 1) : 'function(){ return; }';
         $options['afterValidate']
             = "js:function(form, data, hasError){
                     var vCb = $afterValidateCB;
                     var sCb = $afterSaveCB;
                     if (hasError)
                         return vCb(form, data, hasError);
-                    console.log(data);
                     $.post('{$this->action}', form.serialize(), function(r){
                         sCb(r, form, data);
                     });
@@ -61,6 +49,10 @@ class CActiveAjaxForm extends CActiveForm
         $id=$this->id;
         $cs->registerScript(__CLASS__.'#'.$id,"\$('#$id').yiiactiveform($options);");
         $cs->registerScript(__CLASS__.'#'.$id.'#submit',
-                "$('#$id').submit(function(e) { e.preventDefault(); });");
+                "$('#$id').submit(function(e) {
+                    var osCb = $onSubmitCB;
+                    e.preventDefault();
+                    osCb();
+                });");
     }
 }
