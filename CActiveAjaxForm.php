3e043ce0328c4bac9cd61ec4d25c706565280519
beezee <brian.zeligson@gmail.com>
2013-02-27 22:44:51 -0500

    create jquery.yiiactiveajaxform.js to allow filterPostData callback to affect validation requests in addition to final submission

--- CActiveAjaxForm.php
+++ CActiveAjaxForm.php
@@ -3,10 +3,22 @@
 class CActiveAjaxForm extends CActiveForm
 {
     
+    public $assetsBase = false;
+    public $assetsUrl = false;
+    
     public function __construct($owner=null)
     {
         parent::__construct($owner);
         $this->enableAjaxValidation = true;
+    }
+    
+    public function init()
+    {
+        if (!$this->assetsBase)
+            $this->assetsBase = dirname(__FILE__).DIRECTORY_SEPARATOR.'assets';
+        if (!$this->assetsUrl)
+            $this->assetsUrl = Yii::App()->assetManager->publish($this->assetsBase);
+        parent::init();
     }
     
     public function run()
@@ -22,21 +34,34 @@ class CActiveAjaxForm extends CActiveForm
         
         $options['attributes']=array_values($this->attributes);
         $afterValidateCB = isset($options['afterValidate'])
-            ? preg_replace('/^js\:/', '', $options['afterValidate'], 1) : 'function() {return;}';
+            ? preg_replace('/^js\:/', '', $options['afterValidate'], 1)
+            : 'function() {return;}';
         $afterSaveCB = isset($options['afterSave'])
-            ? preg_replace('/^js\:/', '', $options['afterSave'], 1) : 'function(r) { return; }';
+            ? preg_replace('/^js\:/', '', $options['afterSave'], 1)
+            : 'function(r) { return; }';
         $onSubmitCB = isset($options['onSubmit'])
-            ? preg_replace('/^js\:/', '', $options['onSubmit'], 1) : 'function(){ return; }';
+            ? preg_replace('/^js\:/', '', $options['onSubmit'], 1)
+            : 'function(){ return; }';
+        $filterPostDataCB = isset($options['filterPostData'])
+            ? preg_replace('/^js\:/', '', $options['filterPostData'], 1)
+            : 'function(data) { return data; }';
+        $overrideSubmit = isset($options['overrideSubmit'])
+            ? preg_replace('/^js\:/', '', $options['overrideSubmit'], 1) : 'false';
         $options['afterValidate']
             = "js:function(form, data, hasError){
                     var vCb = $afterValidateCB;
                     var sCb = $afterSaveCB;
+                    var fPdCb = $filterPostDataCB;
+                    var overrideSubmit = $overrideSubmit;
                     if (hasError)
                         return vCb(form, data, hasError);
-                    $.post('{$this->action}', form.serialize(), function(r){
+                    var postData = fPdCb(form.serializeObject());
+                    if (typeof overrideSubmit == 'function')
+                        return overrideSubmit(postData);
+                    $.post('{$this->action}', postData, function(r){
                         sCb(r, form, data);
                     });
-                    }";
+                }";
 
         if($this->summaryID!==null)
                 $options['summaryID']=$this->summaryID;
@@ -45,9 +70,14 @@ class CActiveAjaxForm extends CActiveForm
                 $options['focus']=$this->focus;
 
         $options=CJavaScript::encode($options);
-        $cs->registerCoreScript('yiiactiveform');
+        $cs->registerScriptFile(
+            $this->assetsUrl
+            .DIRECTORY_SEPARATOR.'js'
+            .DIRECTORY_SEPARATOR.'jquery.yiiactiveajaxform.js',
+            CClientScript::POS_END
+        );
         $id=$this->id;
-        $cs->registerScript(__CLASS__.'#'.$id,"\$('#$id').yiiactiveform($options);");
+        $cs->registerScript(__CLASS__.'#'.$id,"\$('#$id').yiiactiveajaxform($options);");
         $cs->registerScript(__CLASS__.'#'.$id.'#submit',
                 "$('#$id').submit(function(e) {
                     var osCb = $onSubmitCB;
